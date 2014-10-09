46f4d12fabf5199a559f1e92e0c858df9a4024d4
Victor Geislinger <VictorLoren@users.noreply.github.com>
2014-10-09 16:23:38 -0700

    Update SAT object
      - Update SATObject.py so it now reads all data from CNF file.
      - Removed the need to check if variable is in dictionary (assumed that the number variables are [1,2..n] where n is the number of variables.

--- SATObject.py
+++ SATObject.py
@@ -4,11 +4,12 @@
 class SATObject(object):
     """
     """
+
     # SATObject has only a list of variables (for refrence) and a clause list
     def __init__(self):
-        # Dictionary in case variable is greater than total number of variables
+        # Dictionary of variable strings (keys) to refernce variable (values)
         self.varDict = {} 
-        # List of clauses represented with tuples of literals
+        # List of clauses represented with sets of literals
         self.clauses = []
 
     # Reads in clause from a line, but assumes every line ends with zero and 
@@ -22,13 +23,35 @@ class SATObject(object):
             isNeg = 1 if (literal[0]=='-') else 0
             # Variable is a literal with (possible) negation removed
             # Add variable to dict as the next integer available for reference
-            if literal[isNeg:] not in self.varDict:
-                self.varDict[literal[isNeg:]] = len(self.varDict) 
+            #if literal[isNeg:] not in self.varDict:
+            #    self.varDict[literal[isNeg:]] = len(self.varDict) 
             # Get recasted variable
             var = self.varDict[literal[isNeg:]]
             # Reform literal from new variable notation (2*v or 2*v+1 if neg) 
             literal = var << 1 | isNeg
-            # Append to the list for this clas
+            # Add literal for this clause
             clause.add(literal)
         # Add this clause into the group of clauses
         self.clauses.append(clause)
+
+    # Takes in an integer and creates dictionary of that number
+    # Ex: setVarDict(3) -> {'1':0, '2':1, '3':2}
+    def setVarDict(self,numOfVars):
+        for v in range(numOfVars):
+            self.varDict[str(v+1)] = v
+
+    def getFromFile(self,cnfFile,hasSet=True):
+        # Create instance of this object
+        # satInstance = cls
+        # Get lines from CNF data and then close file
+        with open(cnfFile) as f:
+            cnfLines = f.readlines()
+        # First line gives us information of data format
+        metaData = cnfLines.pop(0).split()
+        # Get the number of varaibles and clauses specified by file
+        numOfVars, numOfClauses = metaData[2], metaData[3]
+        # Add variables to dictionary for reference
+        self.setVarDict(int(numOfVars))
+        # Add clauses from file (skip the first line of metadata)
+        for line in cnfLines:
+            self.getClauseFromLine(line)
