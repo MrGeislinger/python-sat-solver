e3518a068bcdc78dc9b4b6d76dd54ca24d89af3b
Victor Geislinger <VictorLoren@users.noreply.github.com>
2014-10-11 01:45:01 -0700

    Update Solver object.
      - Add isSolved() function to check if the assignment has satisfied the SAT object. Still has work to be done to check if assignments with SAT's clauses.

--- Solver.py
+++ Solver.py
@@ -20,3 +20,11 @@ Solver(object):
     # Assign assignment varaible a truth value
     def assignVar(self,var,truthValue):
         self.assignment[var] = truthValue
+
+    # Check if assignment has solved SAT object
+    def isSolved(self):
+        # Some variables are not yet assigned
+        if None in self.assignment:
+            return False
+        # Check the all clauses to see if assignment is consistent
+        return True
